2554efdae82b1639e009185f500bd973b351e7c5
Codeassassin-Mpendulo <126480645+Codeassassin-Mpendulo@users.noreply.github.com>
2024-02-08 17:46:46 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.androidappincubator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.androidappincubator"
